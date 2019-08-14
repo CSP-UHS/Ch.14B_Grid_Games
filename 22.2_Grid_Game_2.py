78516c8b03e58607855c38de3ae71b3cacbf44ad
Marc Hermon <hermonm@urbandaleschools.com>
2019-08-14 11:00:03 -0500

    Create 22.2_Grid_Game_2.py

---
+++ 22.2_Grid_Game_2.py
@@ -0,0 +1,27 @@
+# Use this starter code to make your second grid game
+
+import arcade
+
+SW = 500
+SH = 600
+
+class MyGame(arcade.Window):
+
+    def __init__(self, SW, SH, title):
+        super().__init__(SW, SH, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+
+        arcade.start_render()
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        pass
+
+def main():
+
+    window = MyGame(SW, SH, "Grid Game 2")
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
